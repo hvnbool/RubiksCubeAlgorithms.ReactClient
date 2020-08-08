38025123ebac6ce46f556fd5d3f95b6e427ac005
Егор Карташов <edkartashov@edu.hse.ru>
2020-08-08 21:39:36 +0500

    Finish Case component to show case data
    Finishes Case component using Bootstrap Card to show case data

--- src/components/Case.tsx
+++ src/components/Case.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 import { Card } from 'react-bootstrap'
+import CaseModel from "models/case-model"
+import {APICONFIG} from 'services/api-config'
 
-export default function CaseCard() {
+interface TProps {
+  caseModel: CaseModel
+}
+
+export const Case : FunctionComponent<TProps> = ({caseModel}) => {
+  console.log(`${APICONFIG.baseUrl}/images/${caseModel.imageRelativePath}`);
   return (
-    <Card>
-      <Card.Img variant="top" src=""/>
+    <Card style={{ width: '18rem'}}>
+      <Card.Img variant="top" src={`${APICONFIG.baseUrl}/images/${caseModel.imageRelativePath}`}/>
+      <Card.Title>Case {caseModel.id}</Card.Title>
+      <Card.Subtitle>{caseModel.name}</Card.Subtitle>
     </Card>
   )
 }
+
+export default Case;
